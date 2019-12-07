638de314f1913303dd62d93d58469bcf744934d2
Fiju <faixanshirax@gmail.com>
2019-12-07 21:21:20 +0500

    feat: search api integrated with hook

---
+++ src/components/Book.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default ({ imageLinks: { thumbnail }, title, authors = [] }) => (
+  <div className="book">
+    <div className="book-top">
+      <div
+        className="book-cover"
+        style={{
+          width: 128,
+          height: 193,
+          backgroundImage: `url(${thumbnail})`
+        }}
+      ></div>
+      <div className="book-shelf-changer">
+        <select>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{title}</div>
+    <div className="book-authors">{authors.join(", ")}</div>
+  </div>
+);
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchData } from "../hooks";
+import Book from "./Book";
 
-export default ({ onBack, history }) => (
-  <div className="search-books">
-    <div className="search-books-bar">
-      <button className="close-search" onClick={() => history.push("/")}>
-        Close
-      </button>
-      <div className="search-books-input-wrapper">
-        {/*
+export default ({ onBack, history }) => {
+  const [query, setQuery] = useState("");
+  const [books, isFetching] = useSearchData(query);
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <button className="close-search" onClick={() => history.push("/")}>
+          Close
+        </button>
+        <div className="search-books-input-wrapper">
+          {/*
       NOTES: The search from BooksAPI is limited to a particular set of search terms.
       You can find these search terms here:
       https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -15,11 +20,23 @@ export default ({ onBack, history }) => (
       However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
       you don't find a specific author or title. Every search is limited by search terms.
     */}
-        <input type="text" placeholder="Search by title or author" />
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            onChange={e => setQuery(e.target.value.trim())}
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {!isFetching &&
+            books.map((book, bookIndex) => (
+              <li key={bookIndex}>
+                <Book {...book} />
+              </li>
+            ))}
+        </ol>
       </div>
     </div>
-    <div className="search-books-results">
-      <ol className="books-grid"></ol>
-    </div>
-  </div>
-);
+  );
+};
